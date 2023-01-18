ced254683b47d121bc37328895119c9e21a93f16
Yuri-Punzo <yuripunzo@hotmail.it>
2023-01-18 12:38:42 +0100

    #10 Compiled ProductSeeder with faker

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Models\Product;
 
 class ProductSeeder extends Seeder
 {
@@ -12,8 +15,15 @@ class ProductSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $product = new Product();
+            $product->title = $faker->sentence(3);
+            $product->slug = Str::slug($product->title, '-');
+            $product->description = $faker->text(210);
+            //$new_project->cover_image = $faker->image('storage/app/public/placeholders', 600, 300, 'Product', false, true);
+            $product->save();
+        }
     }
 }
