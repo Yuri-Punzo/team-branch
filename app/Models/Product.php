b214919e4f083203eafd8e41245aa82e1ab7ca33
sgraziola <simo.fdl@gmail.com>
2023-01-18 17:39:06 +0100

    WIP Type

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,17 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
 
 class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'slug', 'description'];
+    protected $fillable = ['title', 'slug', 'description', 'type_id'];
 
     public static function generateSlug($title)
     {
         $product_slug = Str::slug($title);
         return $product_slug;
+    }
+
+    /**
+     * Get the type that owns the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
     }
 }
