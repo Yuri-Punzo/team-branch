aa58627eda08f2a30516a335fe721469c9b1dd93
sgraziola <simo.fdl@gmail.com>
2023-01-18 12:46:39 +0100

    Create product table and update up function

--- database/migrations/2023_01_18_112230_create_products_table.php
+++ database/migrations/2023_01_18_112230_create_products_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100)->unique();
+            $table->string('slug');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
